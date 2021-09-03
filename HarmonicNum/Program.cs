a826f01048146fcc50a67d15e77df3feeaef3951
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 23:36:20 +0530

    Harmonic Code Added

---
+++ HarmonicNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HarmonicNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num;
+            double sum = 0.0;
+
+            Console.WriteLine("Enter the Harmonic Number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= num; i++)
+            {
+                Console.WriteLine("1/{0} + ", i);
+                sum += 1 / (float)i;
+            }
+            Console.WriteLine("Sum of Series upto {0} terms : {1}", num, sum);
+
+        }
+    }
+}
