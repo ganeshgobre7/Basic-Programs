a9a9b2e81ce45f7ec796b6a0b2d1d5f3040e2a1d
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 23:03:26 +0530

    Quatient And Remainder Code Added

---
+++ QuotientAndRemainder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace QuotientAndRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, q, r;
+            Console.WriteLine("Enter First Number:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            q = x / y;
+            r = x % y;
+
+            Console.WriteLine("Quatient is:" + q);
+            Console.WriteLine("Remainder is:" + r);
+        }
+    }
+    
+}
