38d8c68d6318770dcb4dff547d2fd3e50c4f009b
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 22:57:03 +0530

    OddEven Code Added

---
+++ OddEven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num, Remainder;
+            Console.WriteLine("Enter the Number:");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            Remainder = Num % 2;
+
+            if (Remainder == 0)
+                Console.WriteLine("Number is Even", Num);
+            else
+                Console.WriteLine("Number is Odd", Num);
+        }
+    }
+}
