30ac0588f4a1790c1af4629354d3bb184884b4b4
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 22:47:01 +0530

    Largest Number Code Added

---
+++ LargestNum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LargestNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, large;
+            Console.WriteLine("Enter First Number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Third Number:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+                large = num1;
+            else if (num2 > num1 && num2 > num3)
+                large = num2;
+            else
+                large = num3;
+
+            Console.WriteLine("Largest Number among {0}, {1} and {2} is {3}", num1, num2, num3, large);
+
+        }
+    }
+}
