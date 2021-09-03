646cd3456297c6246aa4f3e00b576a5d5ca63248
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 23:00:42 +0530

    Swapping Code Added

---
+++ Swapping/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Swapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Temp, Num1, Num2;
+            Console.WriteLine("Enter the First the Number:");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Number:");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+
+            Temp = Num1;
+            Num1 = Num2;
+            Num2 = Temp;
+
+            Console.WriteLine("After Swapping");
+            Console.WriteLine("First Number is:" + Num1);
+            Console.WriteLine("Second Number is:" + Num2);
+        }
+    }
+}
