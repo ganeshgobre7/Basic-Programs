fe18b9a71dc4a2712a385c01d678ecd20c1904d8
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 23:51:28 +0530

    Leap Year Code Added

---
+++ LeapYear/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter the Year:");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 4 == 0) && (year % 100 != 0 || (year % 400 == 0)))
+            {
+                Console.WriteLine("leap Year", +year);
+            }
+            else
+            {
+                Console.WriteLine("Not Leap Year", +year);
+            }
+        }
+    }
+}
